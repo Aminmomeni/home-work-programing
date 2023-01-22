81d4836a49f29d12fc56ecac3bf98e85e177090e
Aminmomeni <116980500+Aminmomeni@users.noreply.github.com>
2023-01-22 23:45:53 +0330

    Add files via upload

---
+++ Yard,To,Meter/Yard,To,Meter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yard_To_Meter
+{
+    class yard
+    {
+        private double Diameter;
+        public double diameter
+        {
+            get { return Diameter; }
+            set { Diameter = value; }
+        }
+        public yard() { }
+        public yard(double d)
+        {
+            this.Diameter = d;
+        }
+        public double conv()
+        {
+            return Diameter / 1.198;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number for convert Meter to Yard: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            yard yrd = new yard(num);
+            double con = yrd.conv();
+            Console.WriteLine(con);
+            Console.ReadKey();
+        }
+    }
+}
