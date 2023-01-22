b5954e2771a2dc739b941471e587ac06e4ab9f78
Aminmomeni <116980500+Aminmomeni@users.noreply.github.com>
2023-01-22 23:42:39 +0330

    Add files via upload

---
+++ payment teacher/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            teacher th = new teacher();
+            th.Id = "123";
+            th.name = "ali";
+            th.family = "ahmadi";
+            th.time = 16;
+            th.payonehour = 23000;
+            Console.WriteLine("person ID :"+th.Id+ "\r\n"+"person Name :" +th.name + "\r\n"+"person Family :" + th.family+ "\r\n"+"peyment :"+ th.payment()+ "\r\n");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ payment teacher/ConsoleApp1/teacher.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class teacher
+    {
+        private string ID;
+        private string Name;
+        private string Family;
+        private int Time;
+        private int payoneHour;
+        public string Id
+        {
+            get { return ID; }
+            set { ID = value; }
+        }
+        public string name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public string family
+        {
+            get { return Family; }
+            set { Family = value; }
+        }
+        public int time
+        {
+            get { return Time; }
+            set
+            {
+                if (value > 0)
+                {
+                    Time = value;
+                }
+                else
+                { Time = 0; }
+            }
+        }
+        public int payonehour
+        {
+            get { return payoneHour; }
+            set
+            {
+                if (value > 0)
+                {
+                    payoneHour = value;
+                }
+                else { payoneHour = 0; }
+            }
+        }
+        public teacher()
+        {
+        }
+        public teacher(string Idd,string Namee, string famiily, int hourr, int payy)
+        {
+            Idd = Id;
+            Namee = name;
+            famiily = family;
+            hourr = time;
+            payy = payonehour;
+        }
+        public long payment()
+        {
+            return Time * payoneHour;
+        }
+    }
+}
